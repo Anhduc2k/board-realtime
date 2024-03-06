7909b3e499bd89db07a5b5142af86c18f29e4bb1
Eng Ger <duclapulga@gmail.com>
2024-03-06 22:06:18 +0700

    fea: create layout dashboard

---
+++ app/(dashboard)/_component/org-sidebar.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+
+const OrgSidebar = () => {
+  return (
+    <div className="hidden lg:flex flex-col sapce-y-6 w-[206px] pl-5 pt-5 bg-red-500">
+      OrgSidebar
+    </div>
+  );
+};
+
+export default OrgSidebar;
