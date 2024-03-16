4533d5d1c21bf92a3b9533b4213570f4b6177428
Eng Ger <duclapulga@gmail.com>
2024-03-16 22:12:59 +0700

    fea: organization sidebar

--- components/hint.tsx
+++ components/hint.tsx
@@ -7,7 +7,7 @@ import {
   
   export interface HintProps {
     label: string;
-    children?: React.ReactNode;
+    children: React.ReactNode;
     side?: "top" | "bottom" | "left" | "right";
     align?: "start" | "center" | "end";
     sideOffset?: number;
