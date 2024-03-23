fec0b7234e22027e9b70002a92a2382faac20fe8
Eng Ger <duclapulga@gmail.com>
2024-03-23 21:41:56 +0700

    fea: List Board

--- app/(dashboard)/_component/board-list.tsx
+++ app/(dashboard)/_component/board-list.tsx
@@ -1,9 +1,13 @@
 "use client";
 
+import React from "react";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 import { EmptyBoards } from "@/app/(dashboard)/_component/empty-board";
 import { EmptyFavorites } from "@/app/(dashboard)/_component/empty-favorites";
 import { EmptySearch } from "@/app/(dashboard)/_component/empty-search";
-import React from "react";
+import BoardCard from "@/app/(dashboard)/_component/board-card";
+import { NewBoardButton } from "@/app/(dashboard)/_component/new-board-button";
 
 interface BoardListProps {
   orgId: string;
@@ -14,8 +18,24 @@ interface BoardListProps {
 }
 
 const BoardList = ({ orgId, query }: BoardListProps) => {
-  const data = [];
+  const data = useQuery(api.boards.get, {orgId});
 
+  if(data == undefined) {
+    return (
+      <div>
+        <h2 className="text-3xl">
+          {query.favorites ? "Favorite boards" : "Team boards"}
+        </h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+          <NewBoardButton orgId={orgId} disabled />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+        </div>
+      </div>
+    )
+  }
   if (!data?.length && query.search) {
     return <EmptySearch />
   }
@@ -28,7 +48,20 @@ const BoardList = ({ orgId, query }: BoardListProps) => {
     return <EmptyBoards />
   }
 
-  return <div>{JSON.stringify(query)}</div>;
+  return <div>
+    <h2 className="text-3xl">
+      {query.favorites ? "Favorite boards" : "Team boards"}
+    </h2>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4
+      xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+        <NewBoardButton orgId={orgId}/>
+        {data?.map((board) => (
+          <BoardCard key={board._id} id={board._id} title={board.title} authorId={board.authorId}
+          authorName={board.authorName} imageUrl={board.imageUrl} createdAt={board._creationTime}
+          orgId={board.orgId} isFavorite={false}/>
+        ))}
+    </div>
+  </div>;
 };
 
 export default BoardList;
