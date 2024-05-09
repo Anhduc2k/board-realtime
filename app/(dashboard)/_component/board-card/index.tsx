6cb9b7bc619048f4be95368228f5b6bad8f2b46a
Eng Ger <duclapulga@gmail.com>
2024-05-09 20:59:58 +0700

    fea: favorite function

--- app/(dashboard)/_component/board-card/index.tsx
+++ app/(dashboard)/_component/board-card/index.tsx
@@ -10,6 +10,9 @@ import { Footer } from "@/app/(dashboard)/_component/board-card/footer";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Actions } from "@/app/(dashboard)/_component/actions";
 import { MoreHorizontal } from "lucide-react";
+import { useApiMutation } from "@/app/(dashboard)/_component/use-api-mutation";
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
 
 interface BoardCardProps {
   id: string;
@@ -36,6 +39,18 @@ const BoardCard = ({
   const createdAtLabel = formatDistanceToNow(createdAt, {
     addSuffix: true,
   });
+
+  const { mutate: onFavorite, pending: pendingFavorite } = useApiMutation(api.board.favorite);
+  const { mutate: onUnfavorite, pending: pendingUnfavorite } = useApiMutation(api.board.unfavorite);
+
+  const toggleFavorite = () => {
+    if(isFavorite) {
+      onUnfavorite({id}).catch(()=> toast.error("Failed to unfavorite"))
+    } else {
+      onFavorite({id, orgId}).catch(()=> toast.error("Failed to favorite"))
+    }
+  }
+
   return (
     <Link href={`board/${id}`}>
       <div className="group aspect-[100/127] border rounded-lg flex flex-col justify-between overflow-hidden">
@@ -55,8 +70,8 @@ const BoardCard = ({
           title={title}
           authorLabel={authorLabel}
           createdAtLabel={createdAtLabel}
-          onClick={() => {}}
-          disabled={false}
+          onClick={toggleFavorite}
+          disabled={pendingFavorite || pendingFavorite}
         />
       </div>
     </Link>
--- convex/boards.ts
+++ convex/boards.ts
@@ -1,3 +1,4 @@
+import { favorite } from "./board";
 import { v } from "convex/values";
 
 import { query } from "./_generated/server";
@@ -15,12 +16,28 @@ export const get = query({
       throw new Error("Unauthorized");
     }
 
-    const  boards = await ctx.db
-        .query("boards")
-        .withIndex("by_org", (q) => q.eq("orgId", args.orgId))
-        .order("desc")
-        .collect();
+    const boards = await ctx.db
+      .query("boards")
+      .withIndex("by_org", (q) => q.eq("orgId", args.orgId))
+      .order("desc")
+      .collect();
 
-        return boards;
-    }
+    const boardsWithfavoriteRelation = boards.map((board) => {
+      return ctx.db
+        .query("userFavorites")
+        .withIndex("by_user_board", (q) =>
+          q.eq("userId", identity.subject).eq("boardId", board._id)
+        )
+        .unique()
+        .then((favorite) => {
+          return {
+            ...board,
+            isFavorite: !!favorite,
+          };
+        });
+    });
+
+    const boardsWithFavoriteBoolean = Promise.all(boardsWithfavoriteRelation);
+    return boardsWithFavoriteBoolean;
+  },
 });
